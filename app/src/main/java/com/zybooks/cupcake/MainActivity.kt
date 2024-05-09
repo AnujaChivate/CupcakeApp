eb0493ec9c66035b42f2a461e672ad55925c1bca
Anuja Chivate <anuja.chivate67@gmail.com>
2024-05-08 21:14:19 -0700

    cupcake app

---
+++ app/src/main/java/com/zybooks/cupcake/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zybooks.cupcake
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.zybooks.cupcake.ui.theme.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
